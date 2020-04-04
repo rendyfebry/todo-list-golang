1802f2a1cd61b194f71f16447563afc0ee5774cc
Rendy Febry <rendyfebry@gmail.com>
2020-04-05 01:56:11 +0700

    feat(Command): Give command name and description

--- cmd/complete.go
+++ cmd/complete.go
@@ -24,13 +24,8 @@ import (
 // completeCmd represents the complete command
 var completeCmd = &cobra.Command{
 	Use:   "complete",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Complete a task",
+	Long:  `Mark a task as completed/done.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("complete called")
 	},
--- cmd/sync.go
+++ cmd/sync.go
@@ -24,13 +24,8 @@ import (
 // syncCmd represents the sync command
 var syncCmd = &cobra.Command{
 	Use:   "sync",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Sync data",
+	Long:  `Sync data between local db and remote db.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("sync called")
 	},
